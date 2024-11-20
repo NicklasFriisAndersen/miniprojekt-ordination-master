b70475c3f58b221ace5d33e4c9faeaf92e94af6a
Alexander <alexanderreuter713@gmail.com>
2024-11-20 11:02:58 +0100

    Production branch

--- shared/Model/Ordination.cs
+++ shared/Model/Ordination.cs
@@ -21,7 +21,7 @@ public abstract class Ordination {
     /// Antal hele dage mellem startdato og slutdato. Begge dage inklusive.
     /// </summary>
     public int antalDage() {
-        // TODO: Implement!
+        // TODO: Implement!!
         return -1;
     }
 
